f2211f16ddb8c1ccf8717c067f118dfac021bf50
Andrew2a1 <adam.brz@onet.eu>
2020-08-11 16:56:46 +0200

    Move main.py to root directory

--- gui/main.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-from gui.Application import Application
-
-if __name__ == "__main__":
-    app = Application()
-    app.run()
---
+++ main.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+
+import kivy
+from gui.Application import Application
+
+if __name__ == "__main__":
+    app = Application()
+    app.run()
