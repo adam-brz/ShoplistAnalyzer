19eeb1bd577bc4e25a0fa0acbb07035b70d9d62a
Andrew2a1 <adam.brz@onet.eu>
2020-08-27 16:55:36 +0200

    Add OwnerInfo class

---
+++ OwnerInfo.py
@@ -0,0 +1,40 @@
+# -*- coding : UTF-8 -*-
+
+from FlyweightFactory import FlyweightFactory
+
+class OwnerInfoFactory(FlyweightFactory):
+    def __new__(self):
+        return FlyweightFactory.__new__(self, OwnerInfoFactory)
+    
+    def create(self, owner, quantity = 0):
+        for info in self.flyweights:
+            if info.getOwner() == owner and info.getQuantity() == quantity:
+                return info
+
+        newInfo = OwnerInfo(owner, quantity)
+        self._addFlyweight(newInfo)
+
+        return newInfo
+
+class OwnerInfo:
+    def __init__(self, owner, quantity = 0):
+        self._info = [owner, quantity]
+
+    def getOwner(self):
+        return self._info[0]
+
+    def setOwner(self, owner):
+        self._info[0] = owner
+
+    def getQuantity(self):
+        return self._info[1]
+
+    def setQuantity(self, quantity):
+        self._info[1] = quantity
+
+    def __iter__(self):
+        return self._info.__iter__()
+
+    def __eq__(self, other):
+        return self._info == other._info
+        
