c14ae43bfc03729bdf1c3787bb1756ad1c0c301f
Andrew2a1 <adam.brz@onet.eu>
2020-08-27 16:56:14 +0200

    Refactor Person and Product classes

--- Person.py
+++ Person.py
@@ -3,23 +3,4 @@
 class Person:
     def __init__(self, name):
         self.name = name
-        self.bought_products = {}
-
-    def addProduct(self, product, count = 1):
-        self.bought_products[product] = count
-
-    def setProductCount(self, product, count = 1):
-        self.bought_products[product] = count
-
-    def getProductCount(self, product):
-        return self.bought_products[product]
-
-    def removeProduct(self, product):
-        if product in self.bought_products:
-            self.bought_products.pop(product)
-
-    def getProducts(self):
-        return set(self.bought_products.keys())
-
-    def getBill(self):
-        return sum(product.price*count for product, count in self.bought_products.items())
+        
--- Product.py
+++ Product.py
@@ -1,6 +1,37 @@
 # -*- coding: UTF-8 -*-
 
+from OwnerGroup import OwnerGroupFactry
+from OwnerInfo import OwnerInfoFactory
+
 class Product:
-    def __init__(self, name, price):
+    def __init__(self, name, price, ownerGroup = None):
         self.name = name
         self.price = price
+        self.ownerGroup = ownerGroup
+
+    def getOwners(self):
+        owners = []
+
+        if self.ownerGroup:
+            owners = self.ownerGroup.getOwners()
+
+        return owners
+
+    def getQuantityForOwner(self, owner):
+        return self.ownerGroup.getQuantityForOwner(owner)
+
+    def setQuantityForOwner(self, owner, quantity):
+        groupFactory = OwnerGroupFactry()
+        infoFactory = OwnerInfoFactory()
+
+        newInfo = infoFactory.create(owner, quantity)
+        self.ownerGroup = groupFactory.updateGroup(self.ownerGroup, newInfo)
+
+    def costPerPerson(self):
+        costs = {}
+
+        if self.ownerGroup:
+            for owner, quantity in self.ownerGroup:
+                costs[owner] = quantity*self.price
+
+        return costs
